c26ca982928afe7031d61a45e0e63886f6f26239
Vignesh Naik <148850345+vignesh-naik-720@users.noreply.github.com>
2024-12-05 20:38:56 +0530

    Update testthat.R

--- page-replacement/tests/testthat.R
+++ page-replacement/tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(disc-scheduling)
+library(PageReplacement)
 
-test_check("disc-scheduling")
+test_check("PageReplacement")
